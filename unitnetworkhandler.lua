bb239b5bd43bca2599bdb082d06af067ddea9991
act2498 <117634708+act2498@users.noreply.github.com>
2023-06-30 16:50:25 +0800

    Update unitnetworkhandler.lua

--- unitnetworkhandler.lua
+++ unitnetworkhandler.lua
@@ -4,15 +4,18 @@ if _G[key] then return else _G[key] = true end
 FadingContour.sync_contour_index = ContourExt and table.index_of(ContourExt.indexed_types, FadingContour.sync_contour)
 
 local fc_original_unitnetworkhandler_sync_contour_remove = UnitNetworkHandler.sync_contour_remove
-function UnitNetworkHandler:sync_contour_state(unit, u_id, type_index, sender_rpc)
+function UnitNetworkHandler:sync_contour_remove(unit, u_id, type_index, sender_rpc)
 	if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_sender(sender) then
 		return
 	end
 
 	if type == FadingContour.sync_contour_index and alive(unit) then
-		unit:contour():fade_color(multiplier / 8)
+		unit:contour():fade_color(0.125)
 		return
 	end
 
 	fc_original_unitnetworkhandler_sync_contour_remove(self, unit, u_id, type_index, sender_rpc)
 end
+--this update split orig funtion to add(state=true) and remove(state=false)
+--since we are checking if not state,so i guess we need use function sync_contour_remove
+--for remove,we also dont have multiplier,but its always 1 in code so i just use 0.125 or 1/8
