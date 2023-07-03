43663e41c775e7189095cfab1ae499f1a63c89d8
act2498 <117634708+act2498@users.noreply.github.com>
2023-07-03 10:52:00 +0800

    im idiot
    i cant believe i forget this

--- unitnetworkhandler.lua
+++ unitnetworkhandler.lua
@@ -5,7 +5,7 @@ FadingContour.sync_contour_index = ContourExt and table.index_of(ContourExt.inde
 
 local fc_original_unitnetworkhandler_sync_contour_remove = UnitNetworkHandler.sync_contour_remove
 function UnitNetworkHandler:sync_contour_remove(unit, u_id, type_index, sender_rpc)
-	if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_sender(sender) then
+	if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_sender(sender_rpc) then
 		return
 	end
 
